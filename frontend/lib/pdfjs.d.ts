2a98651a83f03958147149030ab552eefc349bf8
thnorty <ototatk@gmal.com>
2026-05-05 13:02:33 +0300

    Remove accidental root package files and unused pdfjs declarations

--- frontend/lib/pdfjs.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'pdfjs-dist/build/pdf.js' {
-  export const GlobalWorkerOptions: any;
-  export const getDocument: any;
-}
-
-declare module 'pdfjs-dist/build/pdf.worker.js' {
-  export const WorkerMessageHandler: any;
-}
